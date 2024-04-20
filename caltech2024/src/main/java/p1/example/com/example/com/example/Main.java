f7f1ef020b3d6c3d52cad8a5d1d074c23970e64d
Clement Quach <clementcnd@gmail.com>
2024-04-19 22:57:40 -0700

    testing make

---
+++ caltech2024/src/main/java/p1/example/com/example/com/example/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
