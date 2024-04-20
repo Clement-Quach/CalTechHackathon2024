bd4d6004f8e8be26ded6adb91c309ef563e74ea0
whakley <wesleyc1223@gmail.com>
2024-04-20 16:57:21 -0700

    dd

--- caltech2024/src/main/java/GUI.java
+++ caltech2024/src/main/java/GUI.java
@@ -8,33 +8,34 @@ public class GUI {
     //defining
     private JFrame mainFrame; //creates frame
     private ImageIcon icon = new ImageIcon(this.getClass().getResource("/images/icon.png")); //creates icon
-    private JPanel topBar = new JPanel();
     private JButton questLog = new JButton();
     private ImageIcon questIcon = new ImageIcon(this.getClass().getResource("/images/questLog.png"));
-
+    private JLayeredPane display = new JLayeredPane();
+    private ImageIcon backgroundImage = new ImageIcon(this.getClass().getResource("/images/background.png"));
+    private JLabel background = new JLabel();
     //main GUI
     public GUI(){
             // mainFrame settings
         mainFrame = new JFrame(); //creates frame
         mainFrame.setTitle("title"); //sets title
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainFrame.setSize(1600,1600); //sets dimensions
+        mainFrame.setSize(1920,1080); //sets dimensions
         mainFrame.setIconImage(icon.getImage());
         mainFrame.setVisible(true);
         mainFrame.setLayout(new BorderLayout());
             //questlog button
         questLog.setIcon(questIcon);
         questLog.addActionListener(e -> OpenQuestLog());
-        questLog.setPreferredSize(new Dimension(64,64));
-            //top bar
-        topBar.setLayout(new FlowLayout(FlowLayout.LEFT));
-        topBar.setPreferredSize(new Dimension(1600, 100));
-        topBar.add(questLog); 
-        
-        //adding components to frame
-        mainFrame.add(topBar,BorderLayout.NORTH);
-    }
+        questLog.setBounds(0, 0, 64, 64);
+        questLog.setOpaque(false);
 
+        background.setIcon(backgroundImage);
+        background.setBounds(0,-180,1920,1080);
+        //adding components to frame
+        display.add(background,Integer.valueOf(0));
+        display.add(questLog, Integer.valueOf(1));
+        mainFrame.add(display);
+    }
     private void OpenQuestLog(){
         
     }
