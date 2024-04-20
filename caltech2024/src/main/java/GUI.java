d5213c80452cd39610c44f7e54e3f92c16db4ba1
Wesley Chen <wesleyc1223@gmail.com>
2024-04-20 01:04:10 -0700

    GUI

--- caltech2024/src/main/java/GUI.java
+++ caltech2024/src/main/java/GUI.java
@@ -5,5 +5,14 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 public class GUI {
-
+    private JFrame frame; //creates frame
+    private ImageIcon image = new ImageIcon(this.getClass().getResource("/images/icon.png")); //creates icon
+    public GUI(){
+    frame = new JFrame(); //creates frame
+    frame.setTitle("title"); //sets title
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(420,420); //sets dimensions
+    frame.setIconImage(image.getImage());
+    frame.setVisible(true);
+    }
 }
